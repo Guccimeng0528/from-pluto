a59ce7a373394b16d886a8c6237e85de87d27423
Guccimeng0528 <62088681+Guccimeng0528@users.noreply.github.com>
2026-09-12 16:58:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1991,7 +1991,7 @@ function openModal(event) {
 
             modalImage.innerHTML = `
                 <div class="event-image-placeholder">
-                    NF
+                    <img src="images/NF.png" alt="NF">
                 </div>
             `;
         }
